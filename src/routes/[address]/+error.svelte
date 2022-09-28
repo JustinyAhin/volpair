78c1512c57660ffe211b629b3224a3a8d142609b
Justin Ahinon <justiny.ahinon@gmail.com>
2022-09-28 07:31:21 +0200

    Add some styles to the table and the error page

--- src/routes/[address]/+error.svelte
+++ src/routes/[address]/+error.svelte
@@ -5,3 +5,8 @@
 <section class="flex h-screen items-center justify-center">
 	<h1>{$page.status}: {$page.error?.message}</h1>
 </section>
+
+<svelte:head>
+	<title>{$page.error?.message} - Volpair</title>
+	<meta name="description" content={$page.error?.message} />
+</svelte:head>
--- src/routes/[address]/+page.svelte
+++ src/routes/[address]/+page.svelte
@@ -11,13 +11,13 @@
 	<meta name="description" content="View the volume history of {contractAddress} on Volpair" />
 </svelte:head>
 
-<section>
+<section class="my-32">
 	<table>
 		<thead>
 			<tr>
-				<th>Date</th>
-				<th>Time</th>
-				<th>Volume</th>
+				<th scope="col">Date</th>
+				<th scope="col">Time</th>
+				<th scope="col">Volume</th>
 			</tr>
 		</thead>
 		<tbody>
@@ -31,3 +31,29 @@
 		</tbody>
 	</table>
 </section>
+
+<style>
+	table {
+		@apply w-full text-sm text-left text-gray-600 table-auto overflow-x-auto relative shadow-md sm:rounded-lg;
+	}
+
+	thead {
+		@apply text-xs text-gray-800 uppercase bg-gray-50;
+	}
+
+	thead th {
+		@apply py-3 px-6;
+	}
+
+	tbody tr {
+		@apply bg-white border-b;
+	}
+
+	tbody tr td {
+		@apply p-6;
+	}
+
+	tbody tr:nth-child(even) {
+		@apply bg-gray-50;
+	}
+</style>
