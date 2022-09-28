8a7e814b6bc4127477795596f20a888470f69e49
Justin Ahinon <justiny.ahinon@gmail.com>
2022-09-28 07:49:23 +0200

    Update playwright config to only use chromium

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,16 @@ const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'npm run build && npm run preview',
 		port: 4173
-	}
+	},
+	projects: [
+		{
+			name: 'Desktop',
+			use: {
+				browserName: 'chromium',
+				viewport: { width: 1280, height: 720 },
+			}
+		},
+	]
 };
 
 export default config;
