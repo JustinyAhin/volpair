ccfd89116fe830facb1a5eb228870f64d0939441
Justin Ahinon <justiny.ahinon@gmail.com>
2022-09-28 07:33:07 +0200

    Create +error.svelte

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<section class="flex h-screen items-center justify-center">
+	<h1>{$page.status}: {$page.error?.message}</h1>
+</section>
+
+<svelte:head>
+	<title>{$page.error?.message} - Volpair</title>
+	<meta name="description" content={$page.error?.message} />
+</svelte:head>
